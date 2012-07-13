ebb9f1e7c81f7a992de792e3a72ecac3c2dcbd98
tom chen <2215094817@qq.com>
2012-07-13 13:51:47 +0800

    pp

--- helloWorld/src/HelloWorld.java
+++ helloWorld/src/HelloWorld.java
@@ -1,6 +1,6 @@
 
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("hello everyOne! 888  1123");
+		System.out.println("hello everyOne! 888  1123  jjjj ");
 	}
 }
